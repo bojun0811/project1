32db64a618a405e9187670fda624b23f5cee28cc
Bojun0811 <eva0905787440@gmail.com>
2018-05-17 22:57:33 +0800

    first commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int i,total =0;
+	for (i=1;i<=10;i++)
+	{
+	total +=i;
+	}
+	printf("±q1¥[¨ì10=%d\n",total);
+	
+	return 0;
+}
